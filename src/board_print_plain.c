1fdf9f37617042667a0b61527baf1e84ae065ae1
masscreed <36214853+masscreed@users.noreply.github.com>
2018-05-23 01:31:14 +0700

    Add file and edit old file
    Add func print board

---
+++ src/board_print_plain.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "board_print_plain.h"
+
+void print_board(char** ch)
+{
+	int i, j;
+	for (i = 0; i < 8; ++i)
+    {
+		for (j = 0; j < 8; ++j)
+			printf("%c", ch[i][j]);
+        printf("\n");
+    }
+}
---
+++ src/board_print_plain.h
@@ -0,0 +1,1 @@
+void print_board(char** ch);
