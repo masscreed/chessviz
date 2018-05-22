d84ca0a9d047b229dd56de7c98dd07b2568697a0
masscreed <36214853+masscreed@users.noreply.github.com>
2018-05-23 01:27:30 +0700

    Edit main.c
    Create func new_borrd

--- src/main.c
+++ src/main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+char** create_bord(char** ch);
 int main() 
 {
-    int i, j;
-    char ch[8][8] = {
-	"bnrqkrnb",
+	char **ch = NULL;
+	ch = create_bord(ch);
+	
+    return 0;
+}
+
+char** create_bord(char **p)
+{
+	p = (char**) malloc(9 * sizeof(char*));
+    for (int i = 0; i < 9; i++) {
+        p[i] = (char*) malloc(9 * sizeof(char));
+    }
+	char ko[9][9] = { 
+	"bnrqkrnb", 
 	"pppppppp",
 	"________",
 	"________",
 	"________",
 	"________",
 	"PPPPPPPP",
-	"BNRQKRNB",
-    };
-    
-    for (i = 0; i < 8; ++i)
-    {
-	for (j = 0; j < 8; ++j)
-		printf("%c", ch[i][j]);
-        printf("\n");
-    }
-    
-    return 0;
+	"BNRQKRNB",	
+	};
+	for(int i =0; i< 9; i++)
+		for(int j =0;j<9;j++)
+			p[i][j]=ko[i][j];
+	return p;
 }
